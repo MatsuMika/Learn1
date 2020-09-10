c86ae471f5a2b3e1c1d7614ce476dba51fccb8f1
松永実華 <mika@matsunagamikanoMacBook-Pro.local>
2020-09-10 21:50:50 +0900

    [Add]Booker2復習_トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'homes/top'
-  get 'homes/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root to: 'homes#top'
+  get "home/about" => "homes#about"
+  resources :books
+
+  resources :users, only: [:index, :show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_084444) do
+ActiveRecord::Schema.define(version: 2020_09_10_084908) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,16 +27,16 @@ ActiveRecord::Schema.define(version: 2020_09_09_084444) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.text "Introduction"
-    t.string "profile_image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "introduction"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
