ef64eb93924f472c79c0121758519a5deb55d1c4
松永実華 <mika@matsunagamikanoMacBook-Pro.local>
2020-09-12 20:49:40 +0900

    [Add]Booker2復習

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.All
+  	@user = current_user
+  	@book = Book.new
   end
 
   def show
+  	@user = User.find(params[:id])
+  	@books = @user.books
+  	@book = Book.new
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
+
+  def update
+  end
+
 end
+
